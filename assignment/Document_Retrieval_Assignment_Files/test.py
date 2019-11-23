b19f40b2706ec5fa5cd274c34b908c8a6d3081aa
Seel <459745355@qq.com>
2019-11-23 23:56:53 +0000

    Speed up

--- assignment/Document_Retrieval_Assignment_Files/test.py
+++ assignment/Document_Retrieval_Assignment_Files/test.py
@@ -2,6 +2,17 @@ import re
 import time
 import math
 
+def check_dict(dict1,dict2):
+    if len(dict1)!= len(dict2):
+        print("Different")
+        return False
+    for key,value in dict1.items():
+        if dict2[key] != value:
+            print("Different")
+            return False
+    print("The Same")
+    return True
+
 class IndexLoader:
     def __init__(self, indexFile):
         self.index = {}
@@ -23,11 +34,16 @@ query = {'an': 1, 'articles': 1, 'computers': 1, 'deal': 1, 'exist': 1, 'for': 1
 
 class Retrieve:
     # Create new Retrieve object storing index and termWeighting scheme
-    def __init__(self,index, termWeighting):
+    def __init__(self,index, termWeighting, version):
         self.index = index
         self.termWeighting = termWeighting
         self.docLength = self.__getDocLength()
-        self.docMode = self.getDocMode()
+        if version == 1:
+            self.docMode = self.getDocMode_v1()
+        elif version == 2:
+            self.docMode = self.getDocMode_v2()
+        elif version == 3:
+            self.docMode = self.getDocMode_v3()
 
     def __Viewer(description = None):
         def decorate(func):
@@ -42,23 +58,25 @@ class Retrieve:
             return wrapper
         return decorate
 
+    def __tf_mode(self,term,docid):
+        freq = self.index[term].get(docid,0)
+        return freq
+
+    def __bi_mode(self,term,docid):
+        binary = 1 if docid in self.index[term] else 0
+        return binary
+
     @__Viewer()
-    def getDocMode(self):
+    def getDocMode_v1(self):
         doc_mode = dict()
-        if self.termWeighting == 'tf':
-            for docid in range(1,self.docLength+1): # number of all documents
-                mode = 0 # set mode to 0
-                for term in self.index: # all the term appear in all documents
-                    freq = self.index[term].get(docid,0)  # get frequency of the term
-                    mode += freq * freq
-                doc_mode[docid] = math.sqrt(mode) #pow(mode,1/2)
-        elif self.termWeighting == 'binary':
-            for docid in range(1,self.docLength+1): # number of all documents
-                mode = 0 # set mode to 0
-                for term in self.index: # all the term appear in all documents
-                    binary = 1 if docid in self.index[term] else 0
-                    mode += binary * binary
-                doc_mode[docid] = math.sqrt(mode) #pow(mode,1/2)
+        mode_function_dict = {'tf':self.__tf_mode, 'binary':self.__bi_mode}
+        mode_function = mode_function_dict[self.termWeighting]
+        for docid in range(1,self.docLength+1): # number of all documents
+            mode = 0 # set mode to 0
+            for term in self.index: # all the term appear in all documents
+                mode_item = mode_function(term,docid)
+                mode += mode_item * mode_item
+            doc_mode[docid] = math.sqrt(mode) #pow(mode,1/2)
         return doc_mode
 
     # get the total number of documents
@@ -70,12 +88,48 @@ class Retrieve:
             max_index = present_term if present_term > max_index else max_index
         return max_index
 
+    @__Viewer()
+    def getDocMode_v2(self):
+        doc_mode = dict()
+        if self.termWeighting == 'tf':
+            for term,docid_dict in self.index.items():
+                for docid,freq in docid_dict.items():
+                    doc_mode[docid] = doc_mode.get(docid,0) + freq*freq
+        elif self.termWeighting == 'binary':
+            for term,docid_dict in self.index.items():
+                for docid,freq in docid_dict.items():
+                    doc_mode[docid] = doc_mode.get(docid,0) + 1
+        # may don't need to do know
+        # TODO: sqrt later
+        for docid, accu in doc_mode.items():
+            doc_mode[docid] = math.sqrt(accu)
+        return doc_mode
+
+    @__Viewer()
+    def getDocMode_v3(self):
+        doc_mode = dict().fromkeys(range(1,self.docLength+1),0)
+        if self.termWeighting == 'tf':
+            for term,docid_dict in self.index.items():
+                for docid,freq in docid_dict.items():
+                    doc_mode[docid] += freq*freq
+        elif self.termWeighting == 'binary':
+            for term,docid_dict in self.index.items():
+                for docid,freq in docid_dict.items():
+                    doc_mode[docid] += 1
+        # may don't need to do know
+        # TODO: sqrt later
+        for docid, accu in doc_mode.items():
+            doc_mode[docid] = math.sqrt(accu)
+        return doc_mode
+
     # Method performing retrieval for specified query
-    def forQuery(self, query):
+    @__Viewer()
+    def forQuery_v1(self, query):
         candidate_term = query.keys() & self.index.keys()
         candidate_docid = set()
         for term in candidate_term:
             candidate_docid |= self.index[term].keys()
+        return range(10)
         score = dict()
         for docid in candidate_docid:
             qd = 0
@@ -90,7 +144,24 @@ class Retrieve:
         results = sorted(score, key = lambda k: score[k], reverse=True)
         return results[:10]
 
+    @__Viewer()
+    def forQuery(self, query):
+        sellected_index = {k:v for k,v in self.index.items() if k in query}
+        com_mode = dict()#.fromkeys(range(1,t3.docLength+1),0)
+        for term,docid_dict in sellected_index.items():
+            for docid,freq in docid_dict.items():
+                com_mode[docid] = com_mode.get(docid,0) + query[term] * freq
+        for docid,accu in com_mode.items():
+            com_mode[docid] = com_mode[docid] / self.docMode[docid]
+        results = sorted(com_mode, key = lambda k: com_mode[k], reverse=True)
+        return results[:10]
+
 # %%
-t = Retrieve(index = index,termWeighting = 'binary')
-t.docMode
-pow(t.docMode[500],2)
+tw = ['binary','tf'][0]
+t1 = Retrieve(index = index,termWeighting = tw,version=1)
+t2 = Retrieve(index = index,termWeighting = tw,version=2)
+t3 = Retrieve(index = index,termWeighting = tw,version=3)
+check_dict(t1.docMode,t2.docMode)
+check_dict(t2.docMode,t3.docMode)
+
+t3.forQuery(query)
